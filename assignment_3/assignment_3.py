029aea3e75ae9808fc8420de36cd4a6586629243
Elgun <emajidov2018@ada.edu.az>
2019-11-29 01:48:10 +0100

    prediction raises error in the base classifier

--- assignment_3/assignment_3.py
+++ assignment_3/assignment_3.py
@@ -1,67 +1,99 @@
+import copy
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
-from skmultiflow.data import WaveformGenerator
-from skmultiflow.trees import HoeffdingTree
-from skmultiflow.evaluation import EvaluatePrequential
+from skmultiflow.bayes import NaiveBayes
+from skmultiflow.data.waveform_generator import WaveformGenerator
+from sklearn.datasets import load_iris
 
-stream  = WaveformGenerator()
-stream.prepare_for_use()
+base_classifier = NaiveBayes()
 
-
-def create_expert(x, y):
-    clf = GaussianNB()
-    clf.fit(x, y)
-    return clf
+epochs = 30
+experts = {}
+num_experts = 4
 
 
-def normalize(arr):
-    mean = np.mean(arr)
-    for i in len(arr):
-        arr[i] = arr[i] / mean
-    return arr
+def createExpert():
+    return copy.copy(base_classifier)
 
 
-def remove_experts(experts, weights, theta):
-    idx = []
-    for i in len(weights):
-        if (weights[i] < theta):
-            idx.append(i)
-    weights = np.delete(weights, idx)
-    for i in idx:
-        del experts[i]
-    return experts, weights
+def removeExpert(experts, theta):
+    return [exp for exp in experts if experts[exp] >= theta]
 
 
-def dwm(x, y, c, beta, theta, p):
-    expert1 = create_expert(x, y)
-    expert2 = create_expert(x, y)
-    expert3 = create_expert(x, y)
-    expert4 = create_expert(x, y)
+def normalizeWeights(max_weight):
+    exps = experts.keys()
 
-    m = 4
-    experts = [expert1, expert2, expert3, expert4]
-    w = np.ones((m,), dtype=np.float16)
-    labels = np.unique(y)
-    n = len(x)  # number of samples
-    sigmas = {}
-    for i in labels:
-        sigmas[y] = 0
-    for i in range(n):
+    for exp in exps:
+        experts[exp] *= 1 / max_weight
 
-        for j in range(m):
-            l = experts[j].predict(x)
-            if (l != y[i] and i % p == 0):
-                w[j] = beta * w[j]
-            sigmas[y[i]] = sigmas[y[i]] + w[j]
-        L = sigmas.max()
-        if (i % p == 0):
-            w = normalize(w)
-            experts, w = remove_experts(experts, w, theta)
-        if (L != y[i]):
-            m = m + 1
-            expert = create_expert(x, y)
-            experts.append(expert)
-            w = np.append(w, [1])
-        for j in range(m):
-            print("done")
-    # train experts
+    return experts
+
+
+def getExpertPredictions(experts, X):
+    experts = experts.keys()
+    return [exp.predidct(X) for exp in experts]
+
+
+# def train(exp, X, y, classes, beta, theta, period):
+#     for i in range(len(X)):
+#         dwm(X[i:i + 1, :], y[i:i + 1], classes, beta, theta, period)
+
+
+def dwm(X, y, classes, beta, theta, period):
+    predictions = np.zeros(classes)
+    max_weight = 0
+    weakest_expert_weight = 1
+    weakest_expert_index = 0
+    exps = list(experts.keys())
+
+    for i, exp in enumerate(exps):
+        y_hat = exp.predict(X)
+        if np.any(y_hat != y) and epochs % period == 0:
+            experts[exp] *= beta
+
+        predictions[y_hat] += experts[exp]
+        max_weight = max(max_weight, experts[exp])
+
+        if experts[exp] < weakest_expert_weight:
+            weakest_expert_index = i
+            weakest_expert_weight = experts[exp]
+    y_hat = np.array([np.argmax(predictions)])
+
+    if epochs % period == 0:
+        normalizeWeights(max_weight)
+        removeExpert(experts, theta)
+
+        if np.any(y_hat != y):
+            if len(experts) == num_experts:
+                experts.pop(exps[weakest_expert_index])
+            experts[createExpert()] = 1
+        for exp in experts.keys():
+            exp.fit(X, y, classes=classes)
+
+
+def predict(X):
+    predictions = np.array([np.array(exp.predict(X)) * experts[exp] for exp in experts.keys()])
+    sum_weights = sum(experts[exp] for exp in experts.keys())
+    aggregate = np.sum(predictions / sum_weights, axis=0)
+    return (aggregate + 0.5).astype(int)
+
+
+stream = WaveformGenerator(random_state=774, has_noise=True)
+
+for i in range(4):
+    experts[createExpert()] = 1
+
+data = load_iris()
+X = data.data
+y = data.target
+
+classes = 3
+period = 10
+beta = 0.1
+theta = 0.7
+period = 100
+classes = 3
+
+train = X[1:120, :]
+test = X[120:150, :]
+
+dwm(train, y, 3, 0.1, 0.7, 10)
